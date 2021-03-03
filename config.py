c32501bbe8e63ecba9277c4f906d2ddd15a35b43
= <=>
2021-03-03 13:03:53 +0100

    config, tags and metadata

---
+++ config.py
@@ -0,0 +1,16 @@
+## Global Settings ##
+
+title = "Fast(i)ngAPI"
+description = "Fasting and weight tracker built with FastAPI at the backend and Angular at the front."
+version = "0.1"
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users.",
+    },
+    {
+        "name": "fasts",
+        "description": "Manage fasts. All datetime parameters are in UTC and _timezone-naive_",
+    },
+]
