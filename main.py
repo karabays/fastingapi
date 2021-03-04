fc04e4990cae39c1a4ec4ca0eac3cb681e6eb17e
= <=>
2021-03-04 20:23:53 +0100

    starting weightlog

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from typing import List
 
 import uvicorn
 
-from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi import Depends, FastAPI, HTTPException, Request, Header, Security
 from fastapi.responses import JSONResponse
+from fastapi.security.api_key import APIKeyHeader
 
 from sqlalchemy.orm import Session
 
-from .modules import users
-from .modules import fasts
+from .modules import users, fasts, weights
 from .database import database
 from . import config
 
-
 database.Base.metadata.create_all(bind=database.engine)
-
 
 app = FastAPI(
     title=config.title,
@@ -24,6 +22,7 @@ app = FastAPI(
 )
 
 app.include_router(fasts.router, prefix="/fast", tags=["fasts"])
+app.include_router(weights.router, prefix="/weight", tags=["weight"])
 app.include_router(users.router, prefix="/users", tags=['users'])
 
 
